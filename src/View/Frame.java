35a330d2570958c14ebd82b5d4fa5aa471feb16c
Kazaruth <spam.stenfeldt@gmail.com>
2020-04-06 14:22:28 +0200

    Finished the callback functionality.
    Didn't finish updating the View as we won't be designing it that way anyway. While good for testing, it would be a waste of time in this stage of the development.

--- src/View/Frame.java
+++ src/View/Frame.java
@@ -1,6 +1,8 @@
 package View;
 
 import Control.Callback;
+import Entities.Food;
+import Entities.Ingredient;
 
 /**
  * The abstract class each Frame is built around. Contains the methods used by its children.
@@ -13,7 +15,29 @@ public abstract class Frame extends Thread {
     }
 
     public void increaseValue(int i, String s) {
-        callback.increaseValue(i, s);
+        switch (i) {
+            case 1:
+                if (callback.getIngredient(s) != null) {
+                    callback.increaseIngredient(s);
+                } else {
+                    callback.addIngredient(new Ingredient(s));
+                }
+                break;
+            case 2:
+                if (callback.getFood(s) != null) {
+                    callback.increaseFood(s);
+                } else {
+                    callback.addFood(new Food(s));
+                }
+                break;
+            case 3:
+                if (callback.getSupplier(s) == null) {
+                    callback.addSupplier(s);
+                }
+                break;
+            default:
+                break;  // You shouldn't be here.
+        }
     }
 
     public boolean decreaseValue(int i, String s) {
