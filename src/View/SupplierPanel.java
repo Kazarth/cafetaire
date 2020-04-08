cecf7ba6dc063e8b8c743f15743a6ae36e644efc
PaulMoustakas <62744170+PaulMoustakas@users.noreply.github.com>
2020-04-08 17:34:17 +0200

    SupplierPanel
    Added features to supplier panel , and started a general TABLE Class.

--- src/View/SupplierPanel.java
+++ src/View/SupplierPanel.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -16,29 +18,21 @@ import javafx.stage.Stage;
 
 /**
  * Supplier Menu.java
- * The supplier menu.
+ * The supplier menu provides the GUI containing supplier information.
  * @author Paul Moustakas
  * @version 1.0
  */
 
 public class SupplierPanel extends Application {
 
-    private Button  btnAddNewIng;
-    private Button  btnRemoveIng;
-    private Button  btnEditSupp;
-    //Colours
-    private Color   cDeepGreen  = Color.web("#619f81");
-
-    //Layout
+    //Layout Buttons
     String buttonStyle =  (
             "-fx-background-color: #619f81;" +
-            " -fx-border-color: #6B6C6A;" +
             " -fx-text-fill: #FFFFFF;" +
             " -fx-font-family: Segoe UI;" +
             "-fx-font-weight: bold;" +
             "-fx-font-size: 16;"
     );
-
 
     @Override
     public void start(Stage supplierStage) throws Exception {
@@ -49,44 +43,66 @@ public class SupplierPanel extends Application {
         //Top Supplier Bar
         Text textSuppMenu   = new Text();
         Font MenuTitle      = Font.font("Segoe UI", FontWeight.BOLD, FontPosture.REGULAR, 36);
-        textSuppMenu.setFill(cDeepGreen);
+        textSuppMenu.setFill(Paint.valueOf("#619f81"));
         textSuppMenu.setFont(MenuTitle);
         textSuppMenu.setText("SUPPLIERS");
 
         HBox    hBoxTopLblBar   =   new HBox(textSuppMenu);
-        hBoxTopLblBar.setPrefSize(1366, 85);
+        hBoxTopLblBar.setPrefSize(1086, 85);
         hBoxTopLblBar.setAlignment(Pos.CENTER);
         hBoxTopLblBar.setStyle("-fx-background-color: #FFFFFF;");
 
-
         //Middle Supplier Bar
         HBox    hBoxTopMidBar   =   new HBox();
-        hBoxTopMidBar.setPrefSize(1366, 50);
+        hBoxTopMidBar.setPrefSize(1086, 50);
         hBoxTopMidBar.setStyle("-fx-border-color: #6B6C6A; -fx-background-color: #FFFFFF");
 
-        //Button Bar
-        btnAddNewIng    =   new Button("ADD NEW INGREDIENT");
-        btnRemoveIng    =   new Button("REMOVE INGREDIENT");
-        btnEditSupp     =   new Button("EDIT SUPPLIER");
 
-        //Font Buttons
+        //Button Bar LEFT
+        //Buttons ADD, REMOVE, EDIT
+        Button btnAddNewIng = new Button("ADD NEW INGREDIENT");
+        Button btnRemoveIng = new Button("REMOVE INGREDIENT");
+        Button btnEditSupp = new Button("EDIT SUPPLIER");
         btnAddNewIng.setStyle(buttonStyle);
         btnRemoveIng.setStyle(buttonStyle);
         btnEditSupp.setStyle(buttonStyle);
 
+        HBox    hBoxButtonBarLeft   =   new HBox(20, btnAddNewIng, btnRemoveIng, btnEditSupp);
+        hBoxButtonBarLeft.setPrefSize(543, 85);
+        hBoxButtonBarLeft.setStyle("-fx-background-color: #FFFFFF;");
+        hBoxButtonBarLeft.setAlignment(Pos.CENTER);
 
-        HBox    hBoxButtonBar   =   new HBox(20, btnAddNewIng, btnRemoveIng, btnEditSupp);
-        hBoxButtonBar.setPrefSize(1366, 85);
-        hBoxButtonBar.setStyle("-fx-background-color: #FFFFFF;");
-        hBoxButtonBar.setAlignment(Pos.CENTER);
+        //Button Bar Right
+        String search = (
+                "-fx-text-fill: BLACK;" +
+                "-fx-font-family: Segoe UI;" +
+                "-fx-font-weight: bold;" +
+                "-fx-font-size: 16;");
 
+        Label   lblSearch   =   new Label("SEARCH");
+        lblSearch.setStyle(search);
+        TextField textFieldSearch   =   new TextField("search");
+
+        HBox    hBoxButtonBarRight   =   new HBox(20, lblSearch, textFieldSearch);
+        hBoxButtonBarRight.setPrefSize(543, 85);
+        hBoxButtonBarRight.setStyle("-fx-background-color: #FFFFFF;");
+        hBoxButtonBarRight.setAlignment(Pos.CENTER);
+
+        // Button Bar (LEFT & RIGHT)
+        HBox hBoxButtonBar  =    new HBox(hBoxButtonBarLeft, hBoxButtonBarRight);
+
+        // Vertical Box  BARS(TOP Title, MID sup, Button)
         VBox vBoxMainTopBar  =   new VBox(hBoxTopLblBar, hBoxTopMidBar, hBoxButtonBar);
-        vBoxMainTopBar.setPrefSize(1366, 225);
-
+        vBoxMainTopBar.setPrefSize(1086, 225);
         layout.setTop(vBoxMainTopBar);
 
 
-        Scene supplierScene =   new Scene(layout, 1366, 768);
+        // Center Panel for Table
+
+        HBox    hBoxCenterTable =   new HBox();
+
+
+        Scene supplierScene =   new Scene(layout, 1086, 768);
         supplierStage.setScene(supplierScene);
         supplierStage.show();
     }
