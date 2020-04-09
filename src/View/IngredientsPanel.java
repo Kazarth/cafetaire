2fcecb0ba0e7f1f8b1f902e4cc80449a434d3d83
georgGrankvist <georg.granquist@gmail.com>
2020-04-09 16:36:08 +0200

    Update IngredientsPanel.java

--- src/View/IngredientsPanel.java
+++ src/View/IngredientsPanel.java
@@ -19,42 +19,9 @@ import javafx.stage.Stage;
  */
 
 
-
 public class IngredientsPanel extends Application {
 
-    //***** Panes and Layouts *****
-    private BorderPane borderPane;
-    private VBox mainVbox;
-    private HBox topHBox;
-    private HBox midHBox;
-    private HBox bottomHBox;
-    private HBox westHBOx;
-    private HBox eastHBox;
-    private Scene ingredientsScene;
-    private TableView tableView;
 
-    //***** Buttons and components *****
-
-    private Button addIngredients;
-    private Button removeIngredients;
-
-    private TextField searchTextField;
-
-    private Button addButton;
-    private Button removeButton;
-
-    private TableColumn nameColumn;
-    private TableColumn categoryColumn;
-    private TableColumn stockColumn;
-    private TableColumn supplierColumn;
-    private TableColumn selectedColumn;
-
-
-    //***** Styling *****
-
-    private Text titleText;
-    private Text overView;
-    private Color seaDarkGreen;
     private String buttonStyle = (
             "-fx-background-color: #619f81;" +
             " -fx-text-fill: #FFFFFF;" +
@@ -71,44 +38,54 @@ public class IngredientsPanel extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        addIngredients = new Button("ADD NEW INGREDIENT");
+        /** Button instantiation and Configurations */
+
+        Button addIngredients = new Button("ADD NEW INGREDIENT");
         addIngredients.setStyle(buttonStyle);
         addIngredients.setPrefWidth(200);
         addIngredients.setPrefHeight(30);
-        removeIngredients = new Button("REMOVE INGREDIENT");
 
+        Button removeIngredients = new Button("REMOVE INGREDIENT");
         removeIngredients.setStyle(buttonStyle);
         removeIngredients.setPrefHeight(30);
         removeIngredients.setPrefWidth(200);
 
-        titleText = new Text("INGREDIENTS");
-        overView = new Text("OVERVIEW");
-
-        addButton = new Button("ADD");
+        Button addButton = new Button("ADD");
         addButton.setPrefHeight(30);
         addButton.setPrefWidth(100);
         addButton.setStyle(buttonStyle);
 
-        removeButton = new Button("REMOVE");
+         Button removeButton = new Button("REMOVE");
         removeButton.setPrefHeight(30);
         removeButton.setPrefWidth(100);
         removeButton.setStyle(buttonStyle);
 
-        searchTextField = new TextField("SEARCH");
+        /**  Title and overview text configuration */
+        Text titleText = new Text("INGREDIENTS");
+        Color seaDarkGreen = Color.web("#619f81");
+        titleText.setFill(seaDarkGreen);
+        titleText.setFont(Font.font("Segoe UI", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 40));
+        Text overView = new Text("OVERVIEW");
+        overView.setFont(Font.font("Segoe UI", FontWeight.LIGHT, FontPosture.REGULAR, 20));
+        overView.setFill(Color.BLACK);
+
+        /** Searchbar configuration */
+
+        TextField searchTextField = new TextField("SEARCH");
         searchTextField.setPrefHeight(32);
         searchTextField.setPrefWidth(250);
 
-        seaDarkGreen = Color.web("#619f81");
+        /** Ingredient table configuration and design */
 
-        tableView = new TableView();
-        borderPane = new BorderPane();
+        TableView tableView = new TableView();
+        BorderPane borderPane = new BorderPane();
         borderPane.setCenter(tableView);
 
-        nameColumn = new TableColumn("NAME");
-        categoryColumn = new TableColumn("CATEGORY");
-        stockColumn = new TableColumn("STOCK");
-        supplierColumn = new TableColumn("SUPPLIER");
-        selectedColumn = new TableColumn("SELECTED ITEM");
+        TableColumn nameColumn = new TableColumn("NAME");
+        TableColumn categoryColumn = new TableColumn("CATEGORY");
+        TableColumn stockColumn = new TableColumn("STOCK");
+        TableColumn supplierColumn  = new TableColumn("SUPPLIER");
+        TableColumn selectedColumn = new TableColumn("SELECTED ITEM");
 
         tableView.setStyle("-fx-background-color: #FFFFFF");
         tableView.getColumns().addAll(nameColumn,categoryColumn,stockColumn,supplierColumn,selectedColumn);
@@ -119,47 +96,46 @@ public class IngredientsPanel extends Application {
         supplierColumn.setPrefWidth(200);
         selectedColumn.setPrefWidth(250);
 
+        /** LayoutPane configurations and instantiation.
+         *  VBOX, HBOX. */
 
-        mainVbox = new VBox();
+        VBox mainVbox = new VBox();
+        HBox topHBox = new HBox();
+        HBox midHBox = new HBox();
+        HBox bottomHBox = new HBox();
+        HBox westHBOx = new HBox();
+        HBox eastHBox = new HBox();
         borderPane.setTop(mainVbox);
+
         mainVbox.setPrefSize(1366,225);
-
-        topHBox = new HBox();
         topHBox.setPrefSize(1366,85);
-        mainVbox.getChildren().add(topHBox);
-        topHBox.getChildren().add(titleText);
-        topHBox.setAlignment(Pos.CENTER);
-
-        titleText.setFill(seaDarkGreen);
-        titleText.setFont(Font.font("Segoe UI", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 40));
-
-        midHBox = new HBox();
         midHBox.setPrefSize(1366,50);
-        mainVbox.getChildren().add(midHBox);
-        midHBox.setStyle("-fx-background-color: #21252B;");
-        midHBox.setStyle("-fx-border-color: lightgray;");
-        midHBox.getChildren().add(overView);
-        midHBox.setAlignment(Pos.CENTER);
-        overView.setFont(Font.font("Segoe UI", FontWeight.LIGHT, FontPosture.REGULAR, 20));
-        overView.setFill(Color.BLACK);
-
-        bottomHBox = new HBox();
-        westHBOx = new HBox();
-        eastHBox = new HBox();
+        bottomHBox.setPrefSize(1366,85);
         westHBOx.setPrefSize(683,85);
         eastHBox.setPrefSize(683,85);
-        bottomHBox.setPrefSize(1366,85);
+
+        mainVbox.getChildren().add(topHBox);
+        topHBox.getChildren().add(titleText);
+        mainVbox.getChildren().add(midHBox);
+        midHBox.getChildren().add(overView);
         mainVbox.getChildren().add(bottomHBox);
         bottomHBox.getChildren().addAll(westHBOx,eastHBox);
-        bottomHBox.setStyle("-fx-color: #619f81");
-        westHBOx.setAlignment(Pos.CENTER);
         westHBOx.getChildren().addAll(addIngredients,removeIngredients);
-        westHBOx.setSpacing(20);
-        eastHBox.setAlignment(Pos.CENTER);
         eastHBox.getChildren().addAll(searchTextField ,addButton,removeButton);
+
+        topHBox.setAlignment(Pos.CENTER);
+        midHBox.setAlignment(Pos.CENTER);
+        westHBOx.setAlignment(Pos.CENTER);
+        eastHBox.setAlignment(Pos.CENTER);
+
+        midHBox.setStyle("-fx-background-color: #21252B;");
+        midHBox.setStyle("-fx-border-color: lightgray;");
+        bottomHBox.setStyle("-fx-color: #619f81");
+
+        westHBOx.setSpacing(20);
         eastHBox.setSpacing(20);
 
-        ingredientsScene = new Scene(borderPane,1086,768);
+        Scene ingredientsScene = new Scene(borderPane,1086,768);
         primaryStage.setScene(ingredientsScene);
         primaryStage.show();
 
