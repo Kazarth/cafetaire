8e16d9d5ebd033e7d5cae7ccc4519df7378339d0
Kazaruth <spam.stenfeldt@gmail.com>
2020-06-01 20:24:54 +0200

    Finished all but tweaks on Calendar

--- src/code/view/panes/CalendarPane.java
+++ src/code/view/panes/CalendarPane.java
@@ -6,10 +6,10 @@ import code.entities.Styles;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 
 /**
  * A class for showing a schedule of incoming deliveries etc.
@@ -17,76 +17,86 @@ import javafx.scene.layout.VBox;
  * @version 3.0
  */
 public class CalendarPane extends Pane implements EnhancedPane {
+    // TODO: make certain vals global.
+    private Calendar calendar;
+    private FlowPane calendarPane;
+    private VBox mainContainer;
+
     public CalendarPane(Callback callback) {
-        Label title = new Label("CALENDAR");
-        title.setStyle(Styles.getBoxTitle());
+        Text title = new Text("CALENDAR");
+        title.setStyle(Styles.getTitle());
+        title.setFill(Paint.valueOf("#619f81"));
 
-        HBox titleBox = new HBox();
-        titleBox.setStyle(
-                "-fx-alignment: top_center;" +
+        HBox titleContainer = new HBox(title);
+        titleContainer.setPrefWidth(1014);
+        titleContainer.setMinHeight(70);
+        titleContainer.setMaxHeight(70);
+        titleContainer.setAlignment(Pos.CENTER);
+        titleContainer.setStyle(
                 "-fx-background-color: #FFFFFF;" +
-                "-fx-background-radius: 20 20 0 0;" +
-                ""
+                        "-fx-background-radius: 20 20 0 0"
         );
-        titleBox.setPrefSize(1014, 100);
-        titleBox.getChildren().add(title);
 
-        HBox hBox_filler = new HBox();
-        hBox_filler.setPrefSize(1014, 40);
-        hBox_filler.setStyle(
+        HBox filler = new HBox();
+        filler.setPrefWidth(1014);
+        filler.setMinHeight(35);
+        filler.setMaxHeight(35);
+        filler.setStyle(
                 "-fx-border-color: #6B6C6A;" +
                 "-fx-background-color: #FFFFFF;" +
                 "-fx-border-width: 1 0 1 0"
         );
 
-        Calendar calendar = new Calendar(callback);
-        HBox tableBox = new HBox();
-        tableBox.setStyle(
+        this.calendar = new Calendar(callback);
+        this.calendarPane = new FlowPane();
+        this.calendarPane.setPrefSize(1014, 590);
+        this.calendarPane.setAlignment(Pos.CENTER);
+        this.calendarPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.calendarPane.setStyle(
                 "-fx-alignment: center;" +
-                "-fx-background-color: #EEE;" +
-                ""
+                " -fx-background-color: #fff;" +
+                " -fx-background-radius: 0 0 20 20;"
         );
-        tableBox.setPrefSize(1014, 522);
-        tableBox.getChildren().add(calendar);
+        this.calendarPane.getChildren().add(this.calendar);
 
-        VBox boxContainer = new VBox();
-        boxContainer.setAlignment(Pos.CENTER);
-        boxContainer.getChildren().addAll(titleBox, tableBox);
-
-        HBox mainBox = new HBox();
-        mainBox.setStyle(
-                "-fx-alignment: center;" +
-                "-fx-background-color: #FFFFFF;" +
-                "-fx-background-radius: 20 20 20 20;" +
-                "-fx-padding: 25;"
-        );
-        mainBox.setPrefSize(1014, 695);
-        mainBox.setLayoutX(20);
-        mainBox.setLayoutY(20);
-        mainBox.getChildren().add(boxContainer);
+        this.mainContainer = new VBox();
+        this.mainContainer.setPrefSize(1014, 695);
+        this.mainContainer.setMaxHeight(695);
+        this.mainContainer.setStyle(Styles.getPane());
+        this.mainContainer.setLayoutX(20);
+        this.mainContainer.setLayoutY(20);
+        this.mainContainer.getChildren().addAll(titleContainer, filler, this.calendarPane);
 
         setStyle(Styles.getPane());
         setPrefSize(1054, 736);
-        getChildren().add(mainBox);
+        getChildren().add(this.mainContainer);
 
-        testPopulate(calendar);
+        testPopulate();
     }
 
     public void expand() {
         setPrefWidth(1200);
+
+        mainContainer.setPrefWidth(1160);
+        calendarPane.setPrefWidth(1160);
+        calendar.expand();
     }
 
     public void contract() {
         setPrefWidth(1054);
+
+        calendar.contract();
+        calendarPane.setPrefWidth(1014);
+        mainContainer.setPrefWidth(1014);
     }
 
     public void refresh() {
         // TODO: refresh anything?
     }
 
-    private void testPopulate(Calendar calendar) {
-        calendar.setValue(1, 4, "Wheat from Baerte Kvarn");
-        calendar.setValue(1, 5, "Sugar from Socker Arne AB");
-        calendar.setValue(4, 3, "Beverages from Krönelein");
+    private void testPopulate() {
+        this.calendar.setValue(1, 4, "Wheat from Baerte Kvarn");
+        this.calendar.setValue(1, 5, "Sugar from Socker Arne AB");
+        this.calendar.setValue(4, 3, "Beverages from Krönelein");
     }
 }
